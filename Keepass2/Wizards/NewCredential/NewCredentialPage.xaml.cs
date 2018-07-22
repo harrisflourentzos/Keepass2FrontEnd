6e76d1c3b4f8ec801577110d5a135d81fdc5c7e9
Harris Flourentzos <harris.flourentzos@gmail.com>
2018-07-22 19:25:23 +0300

    re-factored wizard pages

---
+++ Keepass2/Wizards/NewCredential/NewCredentialPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Keepass2.Wizards.NewCredential
+{
+    /// <summary>
+    /// Interaction logic for NewCredentialPage.xaml
+    /// </summary>
+    public partial class NewCredentialPage : Page
+    {
+        public NewCredentialPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
